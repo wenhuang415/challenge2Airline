15fd41bd335349473ef45b3a498520c8c4ee2f07
Cody (Wen) Huang <whuang17@mail.sfsu.edu>
2022-08-11 23:17:14 -0700

    todo filters and options

--- airline/urls.py
+++ airline/urls.py
@@ -1,7 +1,10 @@
 from django.urls import include, path
-from .views import AirlineList
+from .views import AirlineList, AircraftList, CityList, RouteList
 
 
 urlpatterns = [
     path('airline', AirlineList.as_view()),
+    path('aircraft', AircraftList.as_view()),
+    path('city', CityList.as_view()),
+    path('route', RouteList.as_view()),
 ]
